8a783f34a77278858df79b1afbbd62ff41eae2c4
Karthik <karthikkumar.krishnamoorthy@gmail.com>
2020-01-08 18:54:02 +0530

    Updates

---
+++ precache-manifest.10f8fb290284720bd2c0c0bfb9d35037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f243b995bd3083a8e0f7ea622fc0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9803109271058c75e980",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1c91fd424830f82db2ac",
+    "url": "/static/js/2.c40ff0db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c40ff0db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9803109271058c75e980",
+    "url": "/static/js/main.50445ce8.chunk.js"
+  },
+  {
+    "revision": "d9c4b6ccf58511c23c2e",
+    "url": "/static/js/runtime-main.555b311c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
