ed0165e0ea1035032ccf16635289a63cc8279124
Karthik <karthikkumar.krishnamoorthy@gmail.com>
2020-01-09 01:39:33 +0530

    Update profile page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import guy from "./img/home-right.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="home_page">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            <div class="banner_content">
+              <h3 class="text-uppercase">Hello</h3>
+              <h1 class="text-uppercase">I am Karthik</h1>
+              <h5 class="text-uppercase">a software developer</h5>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="home_right_img">
+              <img class="" src={guy} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
